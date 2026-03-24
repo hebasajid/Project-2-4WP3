00a76866a45dc85e1b6c952825843b72f7254150
Heba Sajid <hsajid35467@gmail.com>
2026-03-23 23:53:41 -0600

    initializing and installing dependencies  in api

--- api/server.js
+++ api/server.js
@@ -1,3 +1,19 @@
 const sqlite3 = require("sqlite3").verbose();
 const sqlite = require("sqlite");
 const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+async function startup()
+{
+  db = await sqlite.open({
+    filename: 'api.db',
+    driver: sqlite3.Database
+  });
+  
+  const server = app.listen(3000, function() {
+    console.log("RESTful API listening on port 3000");
+  });
+}
