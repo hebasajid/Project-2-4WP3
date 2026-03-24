1cf4361afba56891523dcb0866f48f35cd09a6cd
Heba Sajid <hsajid35467@gmail.com>
2026-03-24 00:04:18 -0600

    implemented and tested the GET /api route to get seed data.

--- api/server.js
+++ api/server.js
@@ -1,5 +1,4 @@
 const sqlite3 = require("sqlite3").verbose();
-const sqlite = require("sqlite");
 const express = require("express");
 const cors = require('cors');
 
@@ -12,6 +11,23 @@ const db = new sqlite3.Database('./database.sqlite', (err) => {
     if (err) console.error(err.message);
     console.log('Connected to the SQLite database.');
 });
+
+
+//GET ROUTE: gets all expenses from the database route: /api
+
+app.get('/api', (req, res) => {
+    console.log("GET EXPENSES REQUEST RECEIVED");
+    const sql = "SELECT * FROM expenses";
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).json({ error: err.message });
+        } else {
+            res.json(rows); //returning the arrays of expenses as JSON response
+        }
+    });
+});
+
 
 
 //starting server at port 3000 
