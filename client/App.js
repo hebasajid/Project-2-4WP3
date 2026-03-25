0b85a5ce27d14516763edafa61af6f02dec827ff
Heba Sajid <hsajid35467@gmail.com>
2026-03-25 00:28:34 -0600

    storing the input vals from the user and list of expenses from the db

--- client/App.js
+++ client/App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { 
+  StyleSheet, Text, View, TextInput, Button,  //importing necessary components from react native
+  FlatList, ScrollView, TouchableOpacity, Alert 
+} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  //input states for the form fields
+  const [date, setDate] = useState(''); //state for date input field
+  const [amount, setAmount] = useState(''); 
+  const [name, setName] = useState('');
+  const [category, setCategory] = useState('');  
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  //state for storing the list of expenses fetched from  API
+  const [expenses, setExpenses] = useState([]);
+
+  const API_URL = 'http://localhost:3000/api';
+
+  //GET all espenses
+  const fetchExpenses = async () => {
+    try {
+      const response = await fetch(API_URL); //fetching all expenses from  API endpoint
+      const data = await response.json(); //parsing response as json
+      setExpenses(data); //
+    } catch (error) {
+      console.error("Fetch error:", error);
+    }
+  };
+
+  useEffect(() => { 
+    fetchExpenses(); 
+  }, []);
+
+
+}
