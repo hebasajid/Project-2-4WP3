e41159cfb5d02598f6b977f9a4229c92093c6940
Heba Sajid <hsajid35467@gmail.com>
2026-03-27 12:44:17 -0600

    adding inputs for the form including date, value, name, cateogry

--- client/components/ExpenseForm.js
+++ client/components/ExpenseForm.js
@@ -1,3 +1,14 @@
 import React from 'react';
 import { StyleSheet, View, TextInput, Button } from 'react-native';
 
+export default function ExpenseForm({ inputs, setInputs, onAdd }) {
+  return ( 
+    <View style={styles.formContainer}>
+      <TextInput style={styles.input} placeholder="Date (YYYY-MM-DD)" value={inputs.date} onChangeText={(val) => setInputs({...inputs, date: val})} /> // Input for date
+      <TextInput style={styles.input} placeholder="Amount" value={inputs.amount} onChangeText={(val) => setInputs({...inputs, amount: val})} keyboardType="numeric" />
+      <TextInput style={styles.input} placeholder="Item Name" value={inputs.name} onChangeText={(val) => setInputs({...inputs, name: val})} />
+      <TextInput style={styles.input} placeholder="Category" value={inputs.category} onChangeText={(val) => setInputs({...inputs, category: val})} />
+      <Button title="Add Expense" onPress={onAdd} /> // Button to add expense
+    </View>
+  );
+}
